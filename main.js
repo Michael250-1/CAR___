70a5e963b4dfa161d5f8739d7d4e8d770d4cb069
Michael250-1 <79066678+Michael250-1@users.noreply.github.com>
2021-06-14 08:25:21 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+canvas=document.getElementById("MyCanvas");
+ctx=canvas.getContext("2d");
+var Roverwidth=150,Roverheight=90;
+var Roverx=10,Rovery=10;
+rover_image="car veiw.jpg";
+function add(){
+  Rover_image=new Image();
+  Rover_image.onload=uploadrover;
+  Rover_image.src=rover_image;
+
+  bg_image=new Image();
+  bg_image.onload=uploadbg;
+  bg_image.src=background_image;
+}
+
+function uploadbg(){
+    ctx.drawImage(bg_image, 0,0, canvas.width,canvas.height);
+}
+function uploadrover(){
+    ctx.drawImage(Rover_image, Roverx,Rovery, Roverwidth,Roverheight);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e){
+    keypress=e.keyCode;
+    if (keypress=="37") {
+     Left();
+     console.log("left");
+    }
+
+    if (keypress=="38") {
+        Up();
+        console.log("up");
+       }
+
+       if (keypress=="39") {
+        Right();
+        console.log("right");
+       }
+
+       if (keypress=="40") {
+        Down();
+        console.log("down");
+       }
+}
+function Up() {
+    if (Rovery>=0){
+Rovery = Rovery-10;
+uploadbg();
+uploadrover();
+console.log(Roverx, Rovery);
+}
+}
+function Down() {
+    if (Rovery<=700){
+    Rovery = Rovery+10;
+    uploadbg();
+    uploadrover();
+    console.log(Roverx, Rovery);
+}
+    }
+    function Left() {
+        if (Roverx>=0){
+        Roverx = Roverx-10;
+        uploadbg();
+        uploadrover();
+        console.log(Roverx, Rovery);
+    }
+        }
+        function Right() {
+            if (Roverx<=900){
+            Roverx = Roverx+10;
+            uploadbg();
+            uploadrover();
+            console.log(Roverx, Rovery);
+        }
+            }
